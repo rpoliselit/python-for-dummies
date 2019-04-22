cdb64f8dcb535071b5be6afb4f1475a3ca8d62fa
Rafael <rpoliselit@gmail.com>
2019-04-22 08:53:22 -0300

    Exercice with integer number.

---
+++ 005.py
@@ -0,0 +1,8 @@
+"""
+Make a script that reads a integer number
+and prints its predecessor and successor.
+"""
+
+n = int(input('Tyoe an integer number: '))
+
+print("Number: {} \n Predecessor: {} \n Successor: {}".format(n,n-1,n+1))
