d68c16542dce527860baf1bf9cc23ca49f2c1501
rafael <rpoliselit@gmail.com>
2019-10-02 04:49:11 -0300

    Sync and scync count example.

---
+++ asyncio_example_001.py
@@ -0,0 +1,20 @@
+'''
+asyncronous version of count_async.py
+'''
+
+import asyncio
+
+async def count():
+    print('One')
+    await asyncio.sleep(1)
+    print('Two')
+
+async def main():
+    await asyncio.gather(count(),count(),count())
+
+if __name__ == "__main__":
+    from time import perf_counter
+    s = perf_counter()
+    asyncio.run(main())
+    elapsed = perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds")
