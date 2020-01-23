c377905532ba025f63d44bf6c3d2970b0e463b04
Rafael Poliseli Teles <rpoliselit@gmail.com>
2020-01-23 17:15:48 -0300

    Basic operations.

---
+++ numpy/np_ex004.py
@@ -0,0 +1,15 @@
+"""
+Basic operations
+"""
+import numpy as np
+
+a = np.arange(10,60,10)
+b = np.arange(5)
+print(a)
+print(b)
+print(a-b)
+print(a*b)
+print(b/a)
+print(b**2)
+print(np.cos(a))
+print(a<=34)
